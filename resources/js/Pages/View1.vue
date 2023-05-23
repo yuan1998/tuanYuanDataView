47120a6be16ba6cd948dd4b065f35e6ef6eb3e32
yuan1998 <chizhiyueshu@gmail.com>
2023-05-23 13:46:31 +0800

    first commit

--- resources/js/Pages/View1.vue
+++ resources/js/Pages/View1.vue
@@ -3,7 +3,7 @@
               :default-sort="{ prop: 'count', order: 'descending' }"
               border stripe style="width: 100%;height: 100%;">
         <el-table-column prop="owner" label="现场咨询" width="150"/>
-        <el-table-column prop="count" sortable label="接诊" width="50"/>
+        <el-table-column prop="count" sortable label="接诊" width="80"/>
         <el-table-column prop="name" label="顾客"/>
     </el-table>
 </template>
